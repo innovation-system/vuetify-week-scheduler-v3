51fe75aea7b54d9b1cd987964b63fd9186eedc0f
robertsLando <daniel.sorridi@gmail.com>
2025-06-19 18:12:21 +0200

    fix: bundler friendly css

--- vite.build.config.js
+++ vite.build.config.js
@@ -14,11 +14,12 @@ export default defineConfig({
       name: 'VuetifyWeekSchedulerV3',
       // the proper extensions will be added
       fileName: 'vuetify-week-scheduler-v3',
+      formats: ['es'],
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue', 'vuetify'],
+     external: [/^vue/, /^vuetify/], // Exclude Vue and Vuetify and all their submodules
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
@@ -30,5 +31,6 @@ export default defineConfig({
       },
     },
     cssCodeSplit: false, // This keeps CSS together with components
+    
   },
 })
